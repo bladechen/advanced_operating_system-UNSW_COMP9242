054cb8cf572b614ccffccaff184485be6f0b4524
bladechen <475979248@qq.com>
2017-08-12 19:34:03 -0700

    [M3] add vm.c

---
+++ apps/sos/src/vm/vm.c
@@ -0,0 +1,68 @@
+#include "vm.h"
+#include "address_space.h"
+#include "frametable.h"
+#include "vmem_layout.h"
+#include "pagetable.h"
+
+#define verbose 5
+#include "sys/debug.h"
+void vm_bootstrap(void)
+{
+    // TODO, move frametable swap init here.
+}
+void vm_shutdown(void)
+{
+
+
+}
+int vm_fault(vaddr_t vaddr)
+{
+    /* For VM fault triggerd in CODE/DATA address space range,
+     *  we try to solve it by loading contents from elf files. For fault address
+     *  resides in STACK address region, we try to deal with it by allocating
+     *  a new frame for its usage.
+     */
+
+    struct proc* cur_proc = get_current_app_proc();
+    assert (cur_proc != NULL);
+    if (vaddr == 0) // dereference NULL pointer
+    {
+        color_print(ANSI_COLOR_RED, "dereference NULL\n");
+        return EFAULT;
+    }
+    vaddr &= seL4_PAGE_MASK;
+    struct  as_region_metadata *region = as_get_region(cur_proc->p_addrspace, vaddr);
+    if (region == NULL)
+    {
+        color_print(ANSI_COLOR_RED, "can not find region via vaddr 0x%x\n", vaddr);
+        return EFAULT;
+    }
+    if (region->type == IPC)
+    {
+        color_print(ANSI_COLOR_RED, "ipc region should be mapped 0x%x\n", vaddr);
+        assert(0); // should not happend, because we map while start proc.
+        return EFAULT;
+    }
+    // we simply alloc_page and zero them out.
+    if (region->type == STACK || region->type == HEAP)
+    {
+        int ret = as_handle_zerofilled_fault(cur_proc->p_pagetable, region, vaddr);
+        if (ret != 0)
+        {
+            return ret;
+        }
+
+    }
+    else // code, date
+    {
+        assert (region->type == CODE || region->type == DATA);
+        int ret = as_handle_elfload_fault(cur_proc->p_pagetable, region, vaddr);
+        if (ret != 0)
+        {
+            return ret;
+        }
+
+    }
+
+    return 0;
+}
