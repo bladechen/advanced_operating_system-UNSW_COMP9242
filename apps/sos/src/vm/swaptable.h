3119f5c39ed932e32b6b315e4ce0913433429bcb
bladechen <475979248@qq.com>
2017-09-05 05:55:29 -0700

    [M6] change swaptable interface

--- apps/sos/src/vm/swaptable.h
+++ apps/sos/src/vm/swaptable.h
@@ -54,18 +54,19 @@ typedef struct swap_table_entry * swap_table;
 
 
 
-int do_swapout_frame(sos_vaddr_t vaddr,  // frame vaddr
-                     uint32_t* swap_frame_number, // the swap id recorded in page table
-                     uint32_t swap_frame_version
-                     );
+inline int do_swapout_frame(sos_vaddr_t vaddr,  // frame vaddr
+                     uint32_t swap_frame_number_in, // the swap id recorded in page table
+                     uint32_t swap_frame_version,
+                     uint32_t *swap_frame_number_out // the swap id recorded in page table
+                     ){};
 
 
 // return error if swap_frame_number is invalid !
-int do_swapin_frame(sos_vaddr_t vaddr,
+inline int do_swapin_frame(sos_vaddr_t vaddr,
                     uint32_t swap_frame_number,
                     uint32_t* swap_frame_version
-                    );
+                    ){};
 
-int do_free_swap_frame(uint32_t swap_frame_number);
+inline int do_free_swap_frame(uint32_t swap_frame_number){};
 
 #endif
